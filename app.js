c90656e2839f432ff563b3056b9f58b106286bec
NiainaTR <tsantaniaina@gmail.com>
2023-11-06 22:00:49 +0300

    implement delete function

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ const input = document.querySelector('input');
 const div = document.querySelector('.todoList');
 
 
-
-
 form.addEventListener('submit' , e =>{
     e.preventDefault();
 
@@ -25,31 +23,51 @@ form.addEventListener('submit' , e =>{
 })
 
 
+div.addEventListener('click' , e => {
+    
+})
+
+
 function addItemToDom(id , item){
     const element = document.createElement('div');
 
     element.setAttribute('item-id' , id);
     element.classList.add('itemStyle')
-    
+
     element.innerHTML = `
         <input type="checkbox">
         <p>${item}</p>
-        <button>d</button>
     `
-    div.appendChild(element);   
+    const btnDel = document.createElement('button');
+    btnDel.setAttribute('item-id' , id);
+    
+    btnDel.addEventListener('click' , e =>{
+        const idItem = e.currentTarget.getAttribute('item-id');
+
+        removeItemFromDom(idItem);
+        removeItemFromList(idItem);
+        
+        
+    })   
+
+    btnDel.innerHTML = `<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAqklEQVRIS2NkoDFgpLH5DIQscAA6YD4QK+BwyAOgeCIQH8DlUEIWgAyQJ+BLkBpFci34D9WIyyGE5AkGESEDCMljWADTQGncw32M7nWaWwBzOUGvo3kRp3qyI4/aFqC7kBAfbj+xPiBkIMVBNGoBRr4iNUhG44Bg0URxEBGygWQLPgBN5CdkKpr8Q2w1H66cDKoqFxBRm8HsABmegK3qJFRlkugJTOU0twAAUHk4GewN7TMAAAAASUVORK5CYII="/>`
+
+    element.appendChild(btnDel);
+    
+    div.appendChild(element);
+
 }
 
 function addItemToList(id , item){
     listItem.push({id , item})
 }
 
-function removeItemFromDom(id , item){
-    let element = document.querySelector(`id`);
-    
+function removeItemFromDom(id){
+    let element = document.querySelector('[item-id="' + id + '"]');
     div.removeChild(element)
 }
 
-function removeItemFromList(id , item){
+function removeItemFromList(id){
     listItem = listItem.filter((item) => {
         item.id !== id
     })
